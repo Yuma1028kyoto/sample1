1fc8e73df990665ecc262844d6af3fc58f8de23d
DESKTOPC-yukkenan <sasimitabetaina@yahoo.co.jp>
2021-01-13 17:13:45 +0900

    レイアウトの共通化

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -1,39 +1,15 @@
 <template>
-  <section class="container">
+  <section >
     <div>
-      <h3>Nuxt.js のタグが付けられた投稿の一覧</h3>
-      <ul>
-        <li v-for="item in items" :key="item.id">
-          <h4>
-            <span>{{item.title}} </span>
-            <small>
-              <span>by </span>
-              <nuxt-link :to="`/users/${item.user.id}`">
-                {{item.user.id}}
-              </nuxt-link>
-            </small>
-          </h4>
-          <div>{{item.body.slice(0, 130)}}……</div>
-          <p><a target="_blank" :href="item.url">{{item.url}}</a></p>
-        </li>
-      </ul>
+      <h1> Index page </h1>
     </div>
   </section>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
 export default {
-  async asyncData({ store }) {
-    if (store.getters['items'].length) {
-      return;
-    }
-    await store.dispatch('fetchItems');
-  },
-  computed: {
-    ...mapGetters(['items'])
-  }
-};
+  layout: 'home'
+}
 </script>
 
 <style>
